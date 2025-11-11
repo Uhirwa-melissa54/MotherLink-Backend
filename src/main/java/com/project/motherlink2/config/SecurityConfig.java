2a5f24745bb18ccba035c15f9193f145d012002c
Uhirwa-melissa54/ <uhirwamelissa@gmail.com>
2025-11-11 20:38:24 +0200

    Fixed security issue

--- src/main/java/com/project/motherlink2/config/SecurityConfig.java
+++ src/main/java/com/project/motherlink2/config/SecurityConfig.java
@@ -1,29 +1,19 @@
 package com.project.motherlink2.config;
 
-import com.project.motherlink2.Middleware.Auth;
-import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-@AllArgsConstructor
 public class SecurityConfig {
 
-    private final Auth authMiddleware;
-
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrf -> csrf.disable()) // disable CSRF for APIs
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/admins/create", "/api/admins/login").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .addFilterBefore(authMiddleware, UsernamePasswordAuthenticationFilter.class);
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
 
-        return http.build();
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .csrf(csrf -> csrf.disable())
+                .build();
     }
 }
