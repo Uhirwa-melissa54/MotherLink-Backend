71d7510f1e51037497aca85d8c2f9176fc2369b1
Uhirwa-melissa54/ <uhirwamelissa@gmail.com>
2025-11-11 21:09:35 +0200

    added new ambulance route

--- src/main/java/com/project/motherlink2/controller/AmbulanceController.java
+++ src/main/java/com/project/motherlink2/controller/AmbulanceController.java
@@ -1,8 +1,11 @@
 package com.project.motherlink2.controller;
 
+import com.project.motherlink2.Dtos.AmbulanceDto;
 import com.project.motherlink2.model.Ambulance;
 import com.project.motherlink2.model.Organization;
 import com.project.motherlink2.service.AmbulanceService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,8 +19,13 @@ public class AmbulanceController {
     public AmbulanceController(AmbulanceService ambulanceService) {
         this.ambulanceService = ambulanceService;
     }
-
     @GetMapping("/totalAmbulance")
+    public ResponseEntity<?> getTotalAmbulance() {
+        long totalAmbulance = ambulanceService.getTotalAmbulance();
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(new AmbulanceDto(totalAmbulance)); // <- use 'new' to create DTO
+    }
 
     @PostMapping("/create")
     public Ambulance createAmbulance(@RequestBody Ambulance ambulance) {
--- src/main/java/com/project/motherlink2/service/AmbulanceService.java
+++ src/main/java/com/project/motherlink2/service/AmbulanceService.java
@@ -2,23 +2,21 @@ package com.project.motherlink2.service;
 
 import com.project.motherlink2.model.Ambulance;
 import com.project.motherlink2.repository.AmbulanceRepository;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@AllArgsConstructor
+
 public class AmbulanceService {
     private final AmbulanceRepository ambulanceRepository;
-
-    public AmbulanceService(AmbulanceRepository ambulanceRepository) {
-        this.ambulanceRepository = ambulanceRepository;
-    }
-
     public Ambulance saveAmbulance(Ambulance ambulance) {
         return ambulanceRepository.save(ambulance);
     }
-
-    public List<Ambulance> getAmbulances() {
-        return  ambulanceRepository.findAll();
+    public long getTotalAmbulance() {
+        return ambulanceRepository.count();
     }
 }
