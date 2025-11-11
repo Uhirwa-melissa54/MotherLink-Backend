59108d554b5ff5b362be0ce5c13fb3e10d1a31df
Uhirwa-melissa54/ <uhirwamelissa@gmail.com>
2025-11-11 09:02:42 +0200

    fixed abug

--- src/main/java/com/project/motherlink2/Dtos/LoginDto.java
+++ src/main/java/com/project/motherlink2/Dtos/LoginDto.java
@@ -1,8 +1,12 @@
 package com.project.motherlink2.Dtos;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginDto {
     private String email;
     private String password;
