0dabe0371069175e94f4dd887bed8bc7d2765628
Uhirwa-melissa54/ <uhirwamelissa@gmail.com>
2025-11-13 11:37:23 +0200

    updated AppointmentsRepository

--- src/main/java/com/project/motherlink2/repository/AppointmentsRepository.java
+++ src/main/java/com/project/motherlink2/repository/AppointmentsRepository.java
@@ -3,6 +3,9 @@ package com.project.motherlink2.repository;
 import com.project.motherlink2.model.Appointments;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AppointmentsRepository extends JpaRepository<Appointments, Long> {
+    List<Appointments> findByStatus(String status);
 
 }
