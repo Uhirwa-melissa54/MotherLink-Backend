9f5a9905b72ac35571e150c99db9ec861429c42e
Uhirwa-melissa54/ <uhirwamelissa@gmail.com>
2025-11-13 10:48:04 +0200

    added appointment repository

---
+++ src/main/java/com/project/motherlink2/repository/AppointmentsRepository.java
@@ -0,0 +1,8 @@
+package com.project.motherlink2.repository;
+
+import com.project.motherlink2.model.Appointments;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AppointmentsRepository extends JpaRepository<Appointments, Long> {
+
+}
