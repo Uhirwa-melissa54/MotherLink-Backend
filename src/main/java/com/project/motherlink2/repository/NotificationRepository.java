37f46395cf3497f8c6903dd695903793b6fe9564
Uhirwa-melissa54/ <uhirwamelissa@gmail.com>
2025-11-13 10:46:36 +0200

    added notification repository

---
+++ src/main/java/com/project/motherlink2/repository/NotificationRepository.java
@@ -0,0 +1,9 @@
+package com.project.motherlink2.repository;
+
+
+import com.project.motherlink2.model.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+}
