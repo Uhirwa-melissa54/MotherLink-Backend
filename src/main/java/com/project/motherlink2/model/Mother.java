0e2147afe41231b6f19df0f16248025c90c7a588
Uhirwa-melissa54/ <uhirwamelissa@gmail.com>
2025-11-13 07:02:41 +0200

    added mother repository,entity and controller

---
+++ src/main/java/com/project/motherlink2/model/Mother.java
@@ -0,0 +1,47 @@
+package com.project.motherlink2.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "mothers")
+public class Mother {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // works with PostgreSQL BIGSERIAL
+    private Long id;
+
+    private String names;
+
+    private LocalDate dob;
+
+    private String phone;
+
+    private String nationalId;
+
+    private String maritalStatus;
+
+    private String district;
+
+    private String sector;
+
+    private String cell;
+
+    private String village;
+
+    private Integer pregnancyMonths;
+
+    private String trimester;
+
+    private LocalDate deliveryDate;
+
+    private String healthCenter;
+
+    private String insurance;
+}
