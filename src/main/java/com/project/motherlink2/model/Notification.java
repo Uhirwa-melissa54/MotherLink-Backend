de5b2bf6a451f8b475975b2c4bf75a1961911867
Uhirwa-melissa54/ <uhirwamelissa@gmail.com>
2025-11-13 10:58:57 +0200

    added notification service

--- src/main/java/com/project/motherlink2/model/Notification.java
+++ src/main/java/com/project/motherlink2/model/Notification.java
@@ -3,12 +3,14 @@ package com.project.motherlink2.model;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
 public class Notification {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
---
+++ src/main/java/com/project/motherlink2/service/NotificationService.java
@@ -0,0 +1,26 @@
+package com.project.motherlink2.service;
+
+import com.project.motherlink2.model.Notification;
+import com.project.motherlink2.repository.NotificationRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class NotificationService {
+    private final NotificationRepository notificationRepository;
+
+    public NotificationService(NotificationRepository notificationRepository) {
+        this.notificationRepository = notificationRepository;
+    }
+
+    public void sendNotification(String message) {
+        Notification n = new Notification();
+        n.setMessage(message);
+        notificationRepository.save(n);
+    }
+
+    public List<Notification> getAllNotifications() {
+        return notificationRepository.findAll();
+    }
+}
