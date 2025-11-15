a59dd0854db7761b8ef560505556e5c09e0690a0
Uhirwa-melissa54/ <uhirwamelissa@gmail.com>
2025-11-15 21:51:03 +0200

    modified appointment dto

--- src/main/java/com/project/motherlink2/Dtos/AppointementDto.java
+++ src/main/java/com/project/motherlink2/Dtos/AppointementDto.java
@@ -1,4 +1,14 @@
 package com.project.motherlink2.Dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AppointementDto {
+    private String status;
+    private String name;
+    private String motherName;
 }
