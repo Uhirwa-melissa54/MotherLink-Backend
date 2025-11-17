31298ef572dbad1ad92247f8a63a9dad694277f7
Uhirwa-melissa54/ <uhirwamelissa@gmail.com>
2025-11-17 21:39:19 +0200

    added today's appointements in service

--- src/main/java/com/project/motherlink2/service/NotificationService.java
+++ src/main/java/com/project/motherlink2/service/NotificationService.java
@@ -4,6 +4,7 @@ import com.project.motherlink2.model.Notification;
 import com.project.motherlink2.repository.NotificationRepository;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -23,4 +24,12 @@ public class NotificationService {
     public List<Notification> getAllNotifications(String sector) {
         return notificationRepository.findByMotherSectorOrderByCreatedAtDesc(sector);
     }
+
+    public List<Notification> todaysAppointements(String sector) {
+        LocalDateTime startOfDay = LocalDateTime.now().withHour(0).withMinute(0).withSecond(0).withNano(0);
+        LocalDateTime endOfDay = LocalDateTime.now().withHour(23).withMinute(59).withSecond(59) .withNano(999_999_999);
+        return notificationRepository.findTodaysNotificationsBySector(sector,startOfDay,endOfDay);
+    }
+
+
 }
