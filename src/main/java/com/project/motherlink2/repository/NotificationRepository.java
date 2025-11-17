178c7cb88a8ca3186a343a03025ef96c3a554ced
Uhirwa-melissa54/ <uhirwamelissa@gmail.com>
2025-11-17 21:28:18 +0200

    added new notifiction route

--- src/main/java/com/project/motherlink2/repository/NotificationRepository.java
+++ src/main/java/com/project/motherlink2/repository/NotificationRepository.java
@@ -3,8 +3,14 @@ package com.project.motherlink2.repository;
 
 import com.project.motherlink2.model.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    @Query("SELECT n FROM Notification n WHERE n.appointment.mother.sector = :sector ORDER BY n.createdAt DESC")
+    List<Notification> findByMotherSectorOrderByCreatedAtDesc(@Param("sector") String sector);
 
 
 
