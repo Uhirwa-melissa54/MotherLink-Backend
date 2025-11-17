25ac298cd106a818a7a4785060d43bbbd7f49dde
Uhirwa-melissa54/ <uhirwamelissa@gmail.com>
2025-11-17 21:29:39 +0200

    added new notification route

--- src/main/java/com/project/motherlink2/repository/NotificationRepository.java
+++ src/main/java/com/project/motherlink2/repository/NotificationRepository.java
@@ -6,11 +6,22 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
     @Query("SELECT n FROM Notification n WHERE n.appointment.mother.sector = :sector ORDER BY n.createdAt DESC")
     List<Notification> findByMotherSectorOrderByCreatedAtDesc(@Param("sector") String sector);
+
+    @Query("SELECT n FROM Notification n " +
+            "WHERE n.appointment.mother.sector = :sector " +
+            "AND n.createdAt >= :startOfDay AND n.createdAt <= :endOfDay " +
+            "ORDER BY n.createdAt DESC")
+    List<Notification> findTodaysNotificationsBySector(
+            @Param("sector") String sector,
+            @Param("startOfDay") LocalDateTime startOfDay,
+            @Param("endOfDay") LocalDateTime endOfDay
+    );
 
 
 
