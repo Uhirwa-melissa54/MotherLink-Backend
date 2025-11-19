65d91cfe4995ec7b89e5b22c6d2973e654170c67
Uhirwa-melissa54/ <uhirwamelissa@gmail.com>
2025-11-19 16:45:35 +0200

    fixed a bug

--- src/main/java/com/project/motherlink2/controller/CHWController.java
+++ src/main/java/com/project/motherlink2/controller/CHWController.java
@@ -16,7 +16,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/healthworkers")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:5174git ")
 @AllArgsConstructor
 public class CHWController {
 
