7990114ba7a39a9f60f8ca9cbc1a68abccb07bae
Uhirwa-melissa54/ <uhirwamelissa@gmail.com>
2025-11-25 07:54:49 +0200

    added new service

--- src/main/java/com/project/motherlink2/service/UpdaterService.java
+++ src/main/java/com/project/motherlink2/service/UpdaterService.java
@@ -1,4 +1,15 @@
 package com.project.motherlink2.service;
 
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Service;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class UpdaterService {
+    private final SimpMessagingTemplate messagingTemplate;
+
+    public void sendUpdate(String topic, Object payload) {
+        messagingTemplate.convertAndSend("/topic/" + topic, payload);
+    }
 }
