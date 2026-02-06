5f5777a909a9b55f944cb2f2fa6a5e1f7b86db99
Uhirwa-melissa54/ <uhirwamelissa@gmail.com>
2026-02-06 13:00:46 +0200

    modified servent sent controller

--- src/main/java/com/project/motherlink2/controller/SseController.java
+++ src/main/java/com/project/motherlink2/controller/SseController.java
@@ -1,4 +1,20 @@
 package com.project.motherlink2.controller;
 
+import com.project.motherlink2.service.SSeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+@RestController
 public class SseController {
+
+    @Autowired
+    private SSeService sseService;
+
+    @GetMapping(value = "/api/dashboard-updates", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public SseEmitter streamDashboardUpdates() {
+        return sseService.createEmitter();
+    }
 }
